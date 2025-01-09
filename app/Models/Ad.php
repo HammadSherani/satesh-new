acaee1956d5967e666cc43d47a33f0d23c4fecf3
DevHassan101 <hassanshaikh311203@gmail.com>
2025-01-09 22:28:26 +0500

    ad work on user side

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Ad extends Model
 {
     protected $guarded = [];
+
+    public function category() {
+     return $this->belongsTo(Category::class);   
+    }
+    public function state() {
+        return $this->belongsTo(State::class);
+    }
+    public function city() {
+        return $this->belongsTo(City::class);
+    }
 }
--- resources/views/user/ads/index.blade.php
+++ resources/views/user/ads/index.blade.php
@@ -5,15 +5,16 @@
     <div class="flex justify-between items-center">
         <div class="flex items-center w-full lg:w-[550px] rounded-full p-2 border bg-white">
             <div class="mx-2">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#00000">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
+                    fill="#00000">
                     <path
                         d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
-                </svg>        
+                </svg>
             </div>
-            <input type="search" class="w-full border-none focus:ring-0 focus:border-none focus:outline-none">
+            <input type="search" id="mySearch" class="w-full border-none focus:ring-0 focus:border-none focus:outline-none">
         </div>
 
-        <a href="{{route('ads.create')}}" class="rounded bg-indigo-500 text-white px-3 py-2">
+        <a href="{{ route('ads.create') }}" class="rounded bg-indigo-500 text-white px-3 py-2">
             Add Ad
         </a>
     </div>
@@ -34,27 +35,83 @@
     </div> --}}
 
     <div class="mt-5 inline-block overflow-hidden min-w-full rounded-lg shadow">
-        <table class="min-w-full leading-normal">
+        <table class="min-w-full leading-normal" id="myTable">
             <thead>
                 <tr>
                     <th
-                        class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
-                        Name
+                    class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                    #
                     </th>
                     <th
                         class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
-                        Email
+                        Title
+                    </th>
+                    <th
+                        class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                        Description
+                    </th>
+                    <th
+                        class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                        Category
+                    </th>
+                    <th
+                        class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                        State
+                    </th>
+                    <th
+                        class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                        City
+                    </th>
+                    <th
+                        class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                        Status
+                    </th>
+                    <th
+                        class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                        Action
                     </th>
                 </tr>
             </thead>
             <tbody>
-                @foreach ($ads as $ad)
+                @foreach ($ads as $i => $ad)
                     <tr>
                         <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                            <p class="text-gray-900 whitespace-no-wrap">{{ $ad->name }}</p>
+                            {{ $i + 1 }}
                         </td>
                         <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
-                            <p class="text-gray-900 whitespace-no-wrap">{{ $ad->email }}</p>
+                            <p class="text-gray-900 whitespace-no-wrap">{{ $ad->title }}</p>
+                        </td>
+                        <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                            <p class="text-gray-900 whitespace-no-wrap">{{ $ad->description }}</p>
+                        </td>
+                        <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                            <p class="text-gray-900 whitespace-no-wrap">{{ $ad->category->name ?? '' }}</p>
+                        </td>
+                        <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                            <p class="text-gray-900 whitespace-no-wrap">{{ $ad->state->name ?? '' }}</p>
+                        </td>
+                        <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                            <p class="text-gray-900 whitespace-no-wrap">{{ $ad->city->name ?? '' }}</p>
+                        </td>
+                        <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                            @if ($ad->status == 'pending')
+                                <p
+                                    class="whitespace-no-wrap bg-yellow-500 text-white rounded text-center p-2 font-bold capitalize">
+                                    {{ $ad->status }}</p>
+                            @elseif($ad->status == 'approved')
+                                <p
+                                    class="whitespace-no-wrap bg-green-500 text-white rounded text-center p-2 font-bold capitalize">
+                                    {{ $ad->status }}</p>
+                            @else
+                                <p
+                                    class="whitespace-no-wrap bg-red-500 text-white rounded text-center p-2 font-bold capitalize">
+                                    {{ $ad->status }}</p>
+                            @endif
+                        </td>
+                        <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                            <a href="{{url($ad->category->name . '/' . $ad->category->state . '/' . $ad->category->city)}}" class="text-indigo-500 hover:underline font-semibold">View</a>
+                            <a href="{{route('ads.edit', ['ad' => $ad->id])}}"  class="text-yellow-500 hover:underline font-semibold mx-3">Edit</a>
+                            <a href=""  class="text-red-500 hover:underline font-semibold">Delete</a>
                         </td>
                     </tr>
                 @endforeach
